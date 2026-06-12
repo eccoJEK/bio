d6b3c8b0e8496055e0f117e984e3be1743e89e17
asterJEK <jekmakesmusic@gmail.com>
2026-06-12 20:26:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
   titolo: "JEK",
-  sottotitolo: "45°04'N // 08°12'E",
+  sottotitolo: "trying to figure out how to put music together",
 
   cardTag: "NOW FOCUSING",
-  cardTesto: "Ascolta l'ultimo frammento su YouTube",
+  cardTesto: "stream my latest song now",
   cardLink: "https://youtube.com/@asterJEK",
 
-  link1Label: "YouTube Channel",
+  link1Label: "youtube channel",
   link1Url: "https://youtube.com/@asterJEK",
 
-  link2Label: "Instagram Feed",
+  link2Label: "instagram feed",
   link2Url: "https://instagram.com/tuo_username",
 
   diarioTitolo: "LOG_ESSENCE",
   diarioTesto: `Il giallo emerge solo dove il silenzio si fa più profondo. Nuove tracce in fase di scrittura, tasselli di un mondo ancora da decifrare.`,
 
-  versione: "asterjek // abstract framework 2026"
+  versione: "JEK // links page 2026"
 };
